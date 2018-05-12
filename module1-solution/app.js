8c7bb09e9b81e6b4bae75b2c89586656ede0991e
Vinod Sherikar <s.vinod1989@gmail.com>
2018-05-12 14:01:37 +0530

    Lunch checker assignment

---
+++ module1-solution/app.js
@@ -0,0 +1,32 @@
+(function initializeFunction() {
+  'use strict';
+
+  var lunchCheckApp = angular.module("LunchCheck", []);
+
+  lunchCheckApp.controller("LunchCheckController", controllerFunction );
+
+  controllerFunction.$inject = ['$scope'];
+
+  function controllerFunction($scope) {
+        $scope.messageToUser = "";
+        $scope.checkItems = function () {
+          var stringLunchItems = $scope.lunchItems;
+          if(stringLunchItems == ""){
+            $scope.messageToUser = "Please enter data first";
+          }else {
+            var arrayOfItems = stringLunchItems.split(',');
+
+            var noOfItems = arrayOfItems.length;
+
+            if (noOfItems <= 3) {
+              $scope.messageToUser = "Enjoy!";
+            }else {
+              $scope.messageToUser = "Too much!";
+            }
+          }
+        }
+
+
+  }
+
+})();
